06f264154c1acd4784ba69b081aef0a2418ab8f7
Bond Keevil <me@bondkeevil.ca>
2019-08-03 18:26:01 -0400

    Initial add of pages and scripts directories

---
+++ scripts/background.js
@@ -0,0 +1,3 @@
+/* This script is called when the extension is loaded to do background processing and initialization.
+ * This script could download the various dictionaries used to local storage in the background.
+ */
---
+++ scripts/protocolhandler.js
@@ -0,0 +1,5 @@
+/* This script is called in response to a protocol handler with the format ext+dpr:some data
+ * "some data" is passed in as the value of the "request" variable.
+ */
+
+ 
